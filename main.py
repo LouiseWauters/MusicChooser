6ad655c563b7b66dbe8e1ea94b8d68c581f951c8
Louise <lowa1661@gmail.com>
2023-07-05 12:45:17 +0200

    Converted to gymnasium and stable_baselines3 beta version

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def env_test():
     env = MusicEnv()
     env.reset()
     env.render()
-    done = False
+    terminated = False
+    truncated = False
     import random
-    while not done:
+    while not (terminated or truncated):
         action = random.randint(0, 1)
-        obs, reward, done, info = env.step(action)
+        obs, reward, terminated, truncated, info = env.step(action)
+        obs['song_bpm'] = 0
         env.render()
 
 
--- music_env.py
+++ music_env.py
@@ -1,6 +1,6 @@
 import time
 
-import gym
+import gymnasium as gym
 import numpy as np
 
 from music_player import MusicPlayer
@@ -22,7 +22,8 @@ class MusicEnv(gym.Env):
         self.max_song_bpm = max_song_bpm
         self.max_heart_bpm = max_heart_bpm
         self.observations = ['song_bpm', 'heart_bpm']
-        self.observation_space = gym.spaces.MultiDiscrete([max_song_bpm, max_heart_bpm])
+        self.observation_space = gym.spaces.Dict({'heart_bpm': gym.spaces.Discrete(300),
+                                                  'song_bpm': gym.spaces.Discrete(300)})
         self.log = ''
         self.max_steps = max_steps
         self.steps_left = max_steps
@@ -42,25 +43,25 @@ class MusicEnv(gym.Env):
             # pulse sensor is not in use
             self.pulse_sensor_thread = MockSensor(max_bpm=max_heart_bpm)
             print("Pulse sensor is not connected. Mock data is used.")
-
-        print("Waiting for pulse sensor data...")
-        while self.pulse_sensor_thread.get_bpm() == 0:
-            time.sleep(1)
-            # TODO could be never ending
+        else:
+            print("Waiting for pulse sensor data...")
+            while self.pulse_sensor_thread.get_bpm() == 0:
+                time.sleep(1)
+                # TODO could be never ending
 
     def observation(self):
         return np.array([self.state[observation] for observation in self.observations])
 
-    def reset(self):
+    def reset(self, seed=None, options=None):
         self.state = {'song_bpm': None, 'heart_bpm': int(self.pulse_sensor_thread.get_bpm())}
         self.set_next_song()
         self.steps_left = self.max_steps
         self.songs_left = self.songs_per_episode
         self.log = self.state_representation()
-        return self.observation()
+        return self.state.copy(), {}
 
     def step(self, action: int):
-        previous_state = self.observation()
+        previous_state = self.state.copy()
 
         # Do selected action
         self.actions[action]()
@@ -75,7 +76,7 @@ class MusicEnv(gym.Env):
         reward = - distance(self.goal, heart_bpm)
         self.log += f'Reward: {reward}\n'
 
-        done = False
+        terminated = False
 
         if distance(self.goal, self.state['heart_bpm']) < 5:
             reward = 0
@@ -83,14 +84,14 @@ class MusicEnv(gym.Env):
             self.log += 'Goal rate reached.\n'
 
         if self.songs_left == 0:
-            done = True
+            terminated = True
             self.log += 'End of episode.\n'
 
         self.steps_left -= 1
-        done = done or self.steps_left <= 0
+        truncated = self.steps_left <= 0
 
         self.log += self.state_representation(previous_state=previous_state)
-        return self.observation(), reward, done, {}
+        return self.state.copy(), reward, terminated, truncated, {}
 
     def close(self):
         pass
@@ -114,11 +115,11 @@ class MusicEnv(gym.Env):
 
     def state_representation(self, previous_state=None):
         rep_str = ''
-        for i, observation in enumerate(self.observations):
+        for observation in self.observations:
             rep_str += f'{observation}: {self.state[observation]} BPM '
             if previous_state is not None:
-                rep_str += '↑' if self.state[observation] > previous_state[i] \
-                    else '↓' if self.state[observation] < previous_state[i] \
+                rep_str += '↑' if self.state[observation] > previous_state[observation] \
+                    else '↓' if self.state[observation] < previous_state[observation] \
                     else '-'
             rep_str += '\n'
         return rep_str
